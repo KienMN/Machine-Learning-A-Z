4bb564f0270e20bec0cf5923dff6658e950b8e08
Kien Mai Ngoc <kienmn97@gmail.com>
2018-03-20 15:55:57 +0700

    Decision Tree Classification

--- Machine Learning A-Z/Part 3 - Classification/Section 19 - Decision Tree Classification/decision_tree_classification.py
+++ Machine Learning A-Z/Part 3 - Classification/Section 19 - Decision Tree Classification/decision_tree_classification.py
@@ -1,4 +1,4 @@
-# Naive Bayes
+# Decision Tree Classification
 
 # Importing the libraries
 import numpy as np
@@ -8,43 +8,43 @@ import pandas as pd
 # Importing the dataset
 dataset = pd.read_csv('Social_Network_Ads.csv')
 X = dataset.iloc[:, 2: 4].values
-y = dataset.iloc[:, 4].values
+y = dataset.iloc[:, 4]. values
 
-# Spliting the dataset into the Training test and the Test set
+# Spliting the dataset into the Traing set and the Test set
 from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
 
-# Features scaling
+# Feature scaling
 from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
 
-# Fitting the Naive Bayes to the Training set
-from sklearn.naive_bayes import GaussianNB
-classifier = GaussianNB()
+# Fitting decision tree classification into the training set
+from sklearn.tree import DecisionTreeClassifier
+classifier = DecisionTreeClassifier(criterion = 'entropy', random_state = 0)
 classifier.fit(X_train, y_train)
 
-# Predicting the test set results
+# Predicting the test set result
 y_pred = classifier.predict(X_test)
 
 # Making the confusion matrix
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
 
-# Visualizing the traing set results
+# Visualizing the training set results
 X_set, y_set = X_train, y_train
-X1, X2 = np.meshgrid(np.arange(X_set[:, 0].min() - 1, X_set[:, 0].max() + 1, step = 0.01),
-                     np.arange(X_set[:, 1].min() - 1, X_set[:, 1].max() + 1, step = 0.01))
+X1, X2 = np.meshgrid(np.arange(X_train[:, 0].min() - 1, X_train[:, 0].max() + 1, step = 0.01),
+                     np.arange(X_train[:, 1].min() - 1, X_train[:, 1].max() + 1, step = 0.01))
 from matplotlib.colors import ListedColormap
 plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
              alpha = 0.75, cmap = ListedColormap(('red', 'green')))
-plt.xlim = (X1.min(), X1.max())
-plt.ylim = (X2.min(), X2.max())
-for i, j in enumerate(np.unique(y_set)):
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate (np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
                 c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('Naive Bayes (Training set)')
+plt.title('Decision Tree Classification (Training set)')
 plt.xlabel('Age')
 plt.ylabel('Estimated salary')
 plt.legend()
@@ -52,17 +52,17 @@ plt.show()
 
 # Visualizing the test set results
 X_set, y_set = X_test, y_test
-X1, X2 = np.meshgrid(np.arange(X_set[:, 0].min() - 1, X_set[:, 0].max() + 1, step = 0.01),
-                     np.arange(X_set[:, 1].min() - 1, X_set[:, 1].max() + 1, step = 0.01))
+X1, X2 = np.meshgrid(np.arange(X_train[:, 0].min() - 1, X_train[:, 0].max() + 1, step = 0.01),
+                     np.arange(X_train[:, 1].min() - 1, X_train[:, 1].max() + 1, step = 0.01))
 from matplotlib.colors import ListedColormap
 plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
              alpha = 0.75, cmap = ListedColormap(('red', 'green')))
-plt.xlim = (X1.min(), X1.max())
-plt.ylim = (X2.min(), X2.max())
-for i, j in enumerate(np.unique(y_set)):
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate (np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
                 c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('Naive Bayes (Test set)')
+plt.title('Decision Tree Classification (Test set)')
 plt.xlabel('Age')
 plt.ylabel('Estimated salary')
 plt.legend()
